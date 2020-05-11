7ff0d574eba926396ff4ef244537bf3368ac9c11
Michael Gomez <michaelgomez@csus.edu>
2020-05-11 05:01:46 -0700

    fixed hexfile path

--- instruction_memory.v
+++ instruction_memory.v
@@ -6,7 +6,7 @@
       reg [7:0] rom[65535:0];  
       initial  
       begin  
-                $readmemh("final_project/myHexFile.hex", rom, 0, 49);
+                $readmemh("myHexFile.hex", rom, 0, 49);
       end  
       assign instruction = {rom[rom_addr+1],rom[rom_addr]};  
  endmodule   
