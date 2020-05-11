71eaccd3dcb70a5538311baf825e84b5537fc6c5
Michael Gomez <michaelgomez@csus.edu>
2020-05-11 05:25:25 -0700

    fixed monitor output and zero out irrelevant memory entries

--- instruction_memory.v
+++ instruction_memory.v
@@ -2,7 +2,7 @@
  module instr_mem(pc, instruction);  
       input     [15:0]         pc;  
       output wire     [15:0]   instruction;
-      wire [3 : 0] rom_addr = pc[4 : 1];  
+      wire [7 : 0] rom_addr = pc[7 : 0];  
       reg [7:0] rom[65535:0];  
       initial  
       begin  
